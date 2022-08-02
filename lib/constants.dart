a65432f9e36fc22c3967160ec508f7c6c7bc8bdf
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-02 15:18:13 +0000

    Specify constant variables and functions to use across the app

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+// Routes
+import 'package:flutter/material.dart';
+
+const String loginRoute = '/login';
+const String registerRoute = '/register';
+
+// scaffold messager alerts
+void showSnackBar(BuildContext context, String message) {
+  ScaffoldMessenger.of(context)
+    ..clearSnackBars()
+    ..showSnackBar(SnackBar(
+      content: Text(message),
+      behavior: SnackBarBehavior.floating,
+    ));
+}
