bd3fe39cd6d92de9c62cbafe4e7ec6bc730ccdcf
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-03 11:38:21 +0000

    Remove provider use in app

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,7 @@ import 'package:packages_flutter/constants.dart';
 import 'package:packages_flutter/pages/views/home/home.dart';
 import 'package:packages_flutter/pages/views/login/login.dart';
 import 'package:packages_flutter/pages/views/register/register.dart';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import 'core/viewModels/auth_view_model.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,25 +13,18 @@ Future<void> main() async {
   final bool isLoggedIn = prefs.getBool("isLoggedIn") ?? false;
 
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => AuthViewModel(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          textTheme: GoogleFonts.senTextTheme(),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        initialRoute: isLoggedIn ? homeRoute : loginRoute,
-        routes: {
-          Login.routeName: (context) => const Login(),
-          Register.routeName: (context) => const Register(),
-          Home.routeName: (context) => const Home(),
-        },
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: GoogleFonts.senTextTheme(),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      initialRoute: isLoggedIn ? homeRoute : loginRoute,
+      routes: {
+        Login.routeName: (context) => const Login(),
+        Register.routeName: (context) => const Register(),
+        Home.routeName: (context) => const Home(),
+      },
     ),
   );
 }
