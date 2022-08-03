3c71eea2ad5aa8fb94c5eb7a48ee42016c85edea
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-03 10:08:15 +0000

    use sharePreferences to check if user is already logged in

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:packages_flutter/constants.dart';
 import 'package:packages_flutter/pages/views/home/home.dart';
 import 'package:packages_flutter/pages/views/login/login.dart';
 import 'package:packages_flutter/pages/views/register/register.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/viewModels/auth_view_model.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final bool isLoggedIn = prefs.getBool("isLoggedIn") ?? false;
+
   runApp(
     MultiProvider(
       providers: [
@@ -15,28 +22,19 @@ void main() {
           create: (_) => AuthViewModel(),
         ),
       ],
-      child: const MyApp(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          textTheme: GoogleFonts.senTextTheme(),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: isLoggedIn ? homeRoute : loginRoute,
+        routes: {
+          Login.routeName: (context) => const Login(),
+          Register.routeName: (context) => const Register(),
+          Home.routeName: (context) => const Home(),
+        },
+      ),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: GoogleFonts.senTextTheme(),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const Login(),
-      routes: {
-        Login.routeName: (context) => const Login(),
-        Register.routeName: (context) => const Register(),
-        Home.routeName: (context) => const Home(),
-      },
-    );
-  }
 }
