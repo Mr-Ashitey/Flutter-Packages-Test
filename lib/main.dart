70c7e2fa3b6f94f6ab973bcb44b1e754956b9c75
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-03 08:48:08 +0000

    remove local session package since it does not work well for me

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:local_session_timeout/local_session_timeout.dart';
 import 'package:packages_flutter/pages/views/home/home.dart';
 import 'package:packages_flutter/pages/views/login/login.dart';
 import 'package:packages_flutter/pages/views/register/register.dart';
@@ -16,49 +15,28 @@ void main() {
           create: (_) => AuthViewModel(),
         ),
       ],
-      child: MyApp(),
+      child: const MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  final _navigatorKey = GlobalKey<NavigatorState>();
-  NavigatorState get _navigator => _navigatorKey.currentState!;
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final sessionConfig = SessionConfig(
-        invalidateSessionForAppLostFocus: const Duration(seconds: 15),
-        invalidateSessionForUserInactiviity: const Duration(seconds: 15));
-    sessionConfig.stream.listen((SessionTimeoutState timeoutEvent) {
-      if (timeoutEvent == SessionTimeoutState.userInactivityTimeout) {
-        // handle user  inactive timeout
-        _navigator
-            .pushReplacement(MaterialPageRoute(builder: (_) => const Login()));
-      } else if (timeoutEvent == SessionTimeoutState.appFocusTimeout) {
-        // handle user  app lost focus timeout
-        _navigator
-            .pushReplacement(MaterialPageRoute(builder: (_) => const Login()));
-      }
-    });
-    return SessionTimeoutManager(
-      sessionConfig: sessionConfig,
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        navigatorKey: _navigatorKey,
-        theme: ThemeData(
-          textTheme: GoogleFonts.senTextTheme(),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: const Login(),
-        routes: {
-          Login.routeName: (context) => const Login(),
-          Register.routeName: (context) => const Register(),
-          Home.routeName: (context) => const Home(),
-        },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: GoogleFonts.senTextTheme(),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: const Login(),
+      routes: {
+        Login.routeName: (context) => const Login(),
+        Register.routeName: (context) => const Register(),
+        Home.routeName: (context) => const Home(),
+      },
     );
   }
 }
