b8239263965de6b589d82d82b2c30fbc284b0f8b
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-03 10:23:17 +0000

    Implement logout and add tab view

--- lib/pages/views/home/home.dart
+++ lib/pages/views/home/home.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:packages_flutter/core/viewModels/auth_view_model.dart';
 
 import '../../../constants.dart';
 
@@ -8,9 +11,55 @@ class Home extends StatelessWidget {
   static const routeName = homeRoute;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+          actions: [
+            IconButton(
+                onPressed: () async {
+                  showDialog(
+                      context: context,
+                      builder: (_) {
+                        return AlertDialog(
+                          title: const Text('Logout'),
+                          content:
+                              const Text('Are you sure you want to logout?'),
+                          actions: [
+                            TextButton(
+                              child: const Text('No'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            TextButton(
+                              child: const Text('Yes'),
+                              onPressed: () async {
+                                await AuthViewModel().logout();
+                                Navigator.pushReplacementNamed(
+                                    context, loginRoute);
+                              },
+                            ),
+                          ],
+                        );
+                      });
+                },
+                icon: const Icon(Icons.logout_rounded))
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.people_alt_rounded)),
+              Tab(icon: Icon(Icons.inventory_rounded)),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+          ],
+        ),
       ),
     );
   }
