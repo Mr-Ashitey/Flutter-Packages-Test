2f1bf0eb4986235782e03679b73539f96eb117a0
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-09 17:01:25 +0000

    Finish register login success and login/register failure sucess and logout function

--- test/unit_tests/authViewModel_test.dart
+++ test/unit_tests/authViewModel_test.dart
@@ -1,8 +1,7 @@
-import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:packages_flutter/core/services/api_request.dart';
 import 'package:packages_flutter/core/viewModels/auth_view_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../helpers/api_mocks.dart';
 
@@ -10,14 +9,48 @@ void main() {
   late APIMocks apiMocks;
   late RequestApi mockRequestApi;
   late AuthViewModel authViewModel;
+  late SharedPreferences mockedPrefs;
 
-  setUp(() {
+  setUp(() async {
     apiMocks = APIMocks();
     mockRequestApi = RequestApi.test(dio: apiMocks.dio);
     authViewModel = AuthViewModel(mockRequestApi);
+
+    mockedPrefs = await SharedPreferences
+        .getInstance(); // get shared preferences instance
   });
-  test('AuthView Model', () async {
-    apiMocks.loginApiSuccessMock();
-    await authViewModel.login('test@example.com', 'password');
+
+  group('Testing all successful network calls from auth view model', () {
+    test('login function test', () async {
+      apiMocks.loginApiSuccessMock();
+      await authViewModel.login('test@example.com', 'password');
+
+      expect(mockedPrefs.getBool('isLoggedIn'), true);
+      expect(mockedPrefs.getString('token'), APIMocks.accessToken['token']);
+    });
+    test('register function test', () async {
+      apiMocks.registerApiSuccessMock();
+      await authViewModel.register('test@example.com', 'password');
+    });
+
+    test("log out function", () async {
+      await authViewModel.logout(); // call log out function
+
+      expect(mockedPrefs.getBool("isLoggedIn"), false);
+      expect(mockedPrefs.getString("token"), isNull);
+    });
+  });
+  group('Testing all unsuccessful network calls from auth view model', () {
+    test('login function test', () async {
+      apiMocks.loginApiFailureMock();
+
+      expectLater(authViewModel.login('test@example.com', 'password'),
+          throwsA('user not found'));
+    });
+    test('register function test', () async {
+      apiMocks.registerApiFailureMock();
+      expectLater(authViewModel.register('test@example.com', 'password'),
+          throwsA('Note: only defined users succeed registration'));
+    });
   });
 }
