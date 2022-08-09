83a88a53738c4a84bb7ac0587aa1c5a993af7d98
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-09 17:23:48 +0000

    write unit tests for resources view model

---
+++ test/unit_tests/reources_viewModel_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:packages_flutter/core/models/resource_model.dart';
+import 'package:packages_flutter/core/services/api_request.dart';
+import 'package:packages_flutter/core/viewModels/resources_view_model.dart';
+
+import '../helpers/api_mocks.dart';
+
+void main() {
+  late APIMocks apiMocks;
+  late RequestApi mockRequestApi;
+  late ResourcesViewModel resourcesViewModel;
+
+  setUp(() {
+    apiMocks = APIMocks();
+    mockRequestApi = RequestApi.test(dio: apiMocks.dio);
+    resourcesViewModel = ResourcesViewModel(mockRequestApi);
+  });
+
+  test('Test success resources view model', () async {
+    apiMocks.resourcesApiSuccessMock();
+    final resources = await resourcesViewModel.getResources();
+    expect(resources, isA<List<Resource>>());
+    expect(resources.length, 1);
+  });
+  test('Test unsuccess resources view model', () async {
+    apiMocks.resourcesApiFailureMock();
+    expectLater(
+      resourcesViewModel.getResources(),
+      throwsA('not found'),
+    );
+  });
+}
