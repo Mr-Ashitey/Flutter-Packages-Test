6ce017d1a6a1ce53620f1ac8c270ddc0d766d4d2
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-16 15:51:57 +0000

    dependency injection of request api

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:packages_flutter/core/services/api_request.dart';
 import 'package:packages_flutter/pages/views/home/home.dart';
 import 'package:packages_flutter/pages/views/login/login.dart';
 import 'package:packages_flutter/pages/views/register/register.dart';
 
 // reusable extension to build all screens
 extension PumpApp on WidgetTester {
-  Future<void> pumpApp(Widget widget) {
+  Future<void> pumpApp(
+    Widget widget,
+    RequestApi? requestApi,
+  ) {
     return pumpWidget(
       MaterialApp(
         home: widget,
         // initialRoute: LoginScreen.routeName,
         routes: {
-          Login.routeName: (context) => const Login(),
+          Login.routeName: (context) => Login(requestApi: requestApi!),
           Register.routeName: (context) => const Register(),
           Home.routeName: (context) => const Home(),
         },
