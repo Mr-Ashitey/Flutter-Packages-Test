d1f31d862e41085b9cc70fb5f77731a15a84f500
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-16 15:27:57 +0000

    make users view model a state notifier

--- lib/core/providers/users_viewModel.dart
+++ lib/core/providers/users_viewModel.dart
@@ -1,11 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../models/user_model.dart';
 import '../services/api_request.dart';
 import '../services/api_status.dart';
 
-class UsersViewModel {
+final usersProvider =
+    StateNotifierProvider((ref) => UsersViewModel(RequestApi()));
+
+class UsersViewModel extends StateNotifier<List<User>> {
   final RequestApi _api;
 
-  UsersViewModel(this._api);
+  UsersViewModel(this._api) : super([]);
 
   // get list of users
   Future<List<User>> getUsers() async {
