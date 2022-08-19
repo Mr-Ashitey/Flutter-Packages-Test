7a0e19a9f2c3b60698f93cca32dc56b7bd8fc159
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-19 11:39:15 +0000

    comment success api status class since its not used

--- lib/core/services/api_status.dart
+++ lib/core/services/api_status.dart
@@ -1,10 +1,10 @@
 //* Code refers to the error or success code
 //* Response/ErrorResponse refers to the object returned
-class Success {
-  int? code;
-  Object? response;
-  Success({this.code, this.response});
-}
+// class Success {
+//   int? code;
+//   Object? response;
+//   Success({this.code, this.response});
+// }
 
 class Failure {
   int? code;
