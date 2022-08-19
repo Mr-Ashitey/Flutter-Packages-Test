527f3304469860b6e028f6e906958aed96d04afc
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-08-19 11:32:25 +0000

    pass mock request api to usersviewmodel

--- test/unit_tests/users_viewModel_test.dart
+++ test/unit_tests/users_viewModel_test.dart
@@ -13,7 +13,7 @@ void main() {
   setUp(() {
     apiMocks = APIMocks();
     mockRequestApi = RequestApi.test(dio: apiMocks.dio);
-    usersViewModel = UsersViewModel();
+    usersViewModel = UsersViewModel(mockRequestApi);
   });
 
   test('Test success users view model', () async {
