2c294b1c81d5a17b9dead25f416fb45e744299e4
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-09-09 17:04:35 +0000

    Null checking

--- lib/core/models/user_model.dart
+++ lib/core/models/user_model.dart
@@ -8,11 +8,11 @@ class User {
   User({this.id, this.email, this.firstName, this.lastName, this.avatar});
 
   User.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    email = json['email'];
-    firstName = json['first_name'];
-    lastName = json['last_name'];
-    avatar = json['avatar'];
+    id = json['id'] ?? "";
+    email = json['email'] ?? "";
+    firstName = json['first_name'] ?? "";
+    lastName = json['last_name'] ?? "";
+    avatar = json['avatar'] ?? "";
   }
 
   Map<String, dynamic> toJson() {
