0ecf3194ec25dff89d88c060d49a53ee6c7c08d3
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-09-12 14:58:13 +0000

    add user button implementation

--- lib/pages/views/home/tabs/users/add_user.dart
+++ lib/pages/views/home/tabs/users/add_user.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:packages_flutter/core/viewModels/users_view_model.dart';
+import 'package:packages_flutter/helpers/constants.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../../core/viewModels/shared_viewModel.dart';
 
 class AddUser extends StatefulWidget {
   const AddUser({Key? key}) : super(key: key);
@@ -20,44 +25,78 @@ class _AddUserState extends State<AddUser> {
 
   @override
   Widget build(BuildContext context) {
+    final usersViewModel = context.read<UsersViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Add User"),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          labelText('Name'),
-          buildTextField(nameController!, "Enter name"),
-          labelText('Job'),
-          buildTextField(jobController!, "Enter job"),
-          ElevatedButton(onPressed: () {}, child: const Text('Add User')),
+          buildTextField(
+              nameController!, TextInputAction.next, "Enter name", 'Name'),
+          const SizedBox(height: 30),
+          buildTextField(
+              jobController!, TextInputAction.done, "Enter job", 'Job'),
+          const SizedBox(height: 50),
+          Center(
+            child: ElevatedButton(
+                onPressed: () async {
+                  // extract name and job text into variables(name, job)
+                  String name = nameController!.text.trim(),
+                      job = jobController!.text.trim();
+
+                  if (name.isEmpty || job.isEmpty) {
+                    showToast('All fields are required', 'error');
+                    return;
+                  }
+
+                  FocusManager.instance.primaryFocus!
+                      .unfocus(); // unfocus keyboard
+                  await usersViewModel
+                      .addUser(name, job)
+                      .then((value) => Navigator.pop(context))
+                      .catchError(
+                          (error) => showToast(error.toString(), 'error'));
+                },
+                child: context.watch<UsersViewModel>().state == ViewState.busy
+                    ? const SizedBox(
+                        height: 10,
+                        width: 10,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 3,
+                          valueColor: AlwaysStoppedAnimation(Colors.white),
+                        ),
+                      )
+                    : const Text(
+                        'Add User',
+                        style: TextStyle(fontSize: 15),
+                      )),
+          ),
         ],
       ),
     );
   }
 
-  Text labelText(String label) => Text(
-        label,
-        style: const TextStyle(
-          fontSize: 20,
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
+  Widget buildTextField(TextEditingController controller,
+      TextInputAction textInputAction, String helperText, String hintText) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        controller: controller,
+        textCapitalization: TextCapitalization.words,
+        cursorColor: Colors.black,
+        textInputAction: textInputAction,
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black54)),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black, width: 1.5),
+          ),
+          helperText: helperText,
         ),
-      );
-
-  TextField buildTextField(
-      TextEditingController controller, String helperText) {
-    return TextField(
-      controller: controller,
-      cursorColor: Colors.black,
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.black54)),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.black, width: 1.5),
-        ),
-        helperText: helperText,
       ),
     );
   }
