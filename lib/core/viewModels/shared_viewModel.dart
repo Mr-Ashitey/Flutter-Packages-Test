3dc5db76ed8e635b283a3bec6807c94be94fa8bf
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-10-17 15:17:00 +0000

    add property to handle displaying of FAB on scroll

--- lib/core/viewModels/shared_viewModel.dart
+++ lib/core/viewModels/shared_viewModel.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 class BaseModel extends ChangeNotifier {
   ViewState _state = ViewState.idle;
+  bool _showFab = true;
+
   ViewState get state => _state;
+  bool get showFab => _showFab;
+
+  void changeFabVisibility() {
+    _showFab = !_showFab;
+    notifyListeners();
+  }
 
   void setState(ViewState viewState) {
     _state = viewState;
