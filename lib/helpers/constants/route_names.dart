18f8f5bfce5b2aaa7f6c5a4f496c4d2fd239f638
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-11-22 10:33:36 +0000

    move route names to constants folder

---
+++ lib/helpers/constants/route_names.dart
@@ -0,0 +1,7 @@
+class RouteNames {
+  static const String homeRoute = '/';
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String addUserRoute = '/add-user';
+  static const String addResourceRoute = '/add-resource';
+}
