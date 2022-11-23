44242888ea458c6196adf1137e00ffe8d41cab20
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-11-23 11:33:36 +0000

    use tabcontroller hooks instead

--- lib/pages/views/home/home.dart
+++ lib/pages/views/home/home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:packages_flutter/core/viewModels/auth_provider/auth_view_model.dart';
 import 'package:packages_flutter/core/viewModels/resource_provider/resources_view_model.dart';
 import 'package:packages_flutter/core/viewModels/users_provider/users_view_model.dart';
@@ -9,36 +10,44 @@ import 'package:packages_flutter/pages/views/home/tabs/resources/resources.dart'
 import 'package:packages_flutter/pages/views/home/tabs/users/users.dart';
 import 'package:provider/provider.dart';
 
-class Home extends StatefulWidget {
+class Home extends HookWidget {
   const Home({Key? key}) : super(key: key);
 
   static const routeName = RouteNames.homeRoute;
 
-  @override
-  State<Home> createState() => _HomeState();
-}
+  // @override
+  // void initState() {
+  //   tabController = TabController(initialIndex: 0, length: 2, vsync: this);
+  //   tabController!.addListener(() {
+  //     // do this to update tabController index on swipe and ontap
+  //     setState(() {});
+  //   });
+  //   super.initState();
+  // }
 
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  TabController? _tabController;
-  @override
-  void initState() {
-    _tabController = TabController(initialIndex: 0, length: 2, vsync: this);
-    _tabController!.addListener(() {
-      // do this to update _tabController index on swipe and ontap
-      setState(() {});
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _tabController!.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   tabController!.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final bool showFab = _tabController!.index == 0
+    final TabController tabController = useTabController(initialLength: 2);
+    final currentIndex = useState(0);
+
+    useEffect(() {
+      tabController.addListener(() {
+        // do this to update tabController index on swipe and ontap
+        currentIndex.value = tabController.index;
+      });
+      return null;
+    });
+    // tabController.addListener(() {
+    //   _index.value = _controller.index;
+    // });
+
+    final bool showFab = currentIndex.value == 0
         ? context.watch<UsersViewModel>().showFab
         : context.watch<ResourcesViewModel>().showFab;
     return Scaffold(
@@ -76,7 +85,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
               icon: const Icon(Icons.logout_rounded))
         ],
         bottom: TabBar(
-          controller: _tabController,
+          controller: tabController,
           indicatorColor: Colors.white,
           tabs: const [
             Tab(icon: Icon(Icons.people_alt_rounded)),
@@ -85,14 +94,14 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         ),
       ),
       body: TabBarView(
-        controller: _tabController,
+        controller: tabController,
         children: const [Users(), Resources()],
       ),
       floatingActionButton: showFab
           ? FloatingActionButton(
               child: const Icon(Icons.add),
               onPressed: () {
-                if (_tabController!.index == 0) {
+                if (currentIndex.value == 0) {
                   Navigator.pushNamed(context, RouteNames.addUserRoute);
                   return;
                 }
