6791cba0a9b9625ecabd2366dee3e6be4e71101b
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-11-23 11:44:59 +0000

    export core and helpers files

---
+++ lib/core/core_export.dart
@@ -0,0 +1,15 @@
+// export core models
+export 'package:packages_flutter/core/models/resource_model.dart';
+export 'package:packages_flutter/core/models/user_model.dart';
+
+// export core services
+export 'package:packages_flutter/core/services/api_request.dart';
+
+// export core utils
+export 'package:packages_flutter/core/utils/dialog.dart';
+
+// export core viewModels
+export 'package:packages_flutter/core/viewModels/auth_provider/auth_view_model.dart';
+export 'package:packages_flutter/core/viewModels/resource_provider/resources_view_model.dart';
+export 'package:packages_flutter/core/viewModels/users_provider/users_view_model.dart';
+export 'package:packages_flutter/core/viewModels/shared_viewModel.dart';
---
+++ lib/helpers/helpers_export.dart
@@ -0,0 +1,3 @@
+export 'package:packages_flutter/helpers/constants/route_names.dart';
+export 'package:packages_flutter/helpers/route_generator.dart';
+export 'package:packages_flutter/helpers/theme.dart';
