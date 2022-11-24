4810e5ebcb815ac331a4035eee86c75317dceeff
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-11-24 12:27:07 +0000

    add pre utils and api status export

--- lib/core/core_export.dart
+++ lib/core/core_export.dart
@@ -4,9 +4,11 @@ export 'package:packages_flutter/core/models/user_model.dart';
 
 // export core services
 export 'package:packages_flutter/core/services/api_request.dart';
+export 'package:packages_flutter/core/services/api_status.dart';
 
 // export core utils
 export 'package:packages_flutter/core/utils/dialog.dart';
+export 'package:packages_flutter/core/offiline/pref_utils.dart';
 
 // export core viewModels
 export 'package:packages_flutter/core/viewModels/auth_provider/auth_view_model.dart';
